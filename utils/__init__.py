7f94f1dfb2f489ff38785093f531808093496e18
SwiTzeX <nanisouhail@gmail.com>
2025-04-24 15:02:05 +0100

    fix issue

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,7 @@
 # scanner/utils/__init__.py
 
-from .tls_scanner import scan_ssl_tls, determine_severity
+from .tls_scanner import scan_ssl_tls
+from .vuln_utils import determine_severity
 from .dns_smtp_icmp import run_nsc_checks
 from .cve_api import query_cve_api
 from .passive_web import passive_web_analysis
